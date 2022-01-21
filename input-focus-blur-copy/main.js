4d04112454495e845c63ba5d00790c1a088a9592
avel-front <vp.kalashnikov@mail.ru>
2022-01-21 14:03:16 +0300

    added changes in main.js and main.css

--- input-focus-blur-copy/main.js
+++ input-focus-blur-copy/main.js
@@ -2,11 +2,12 @@
 let divElem = document.createElement('div');
 document.body.append(divElem)
 
-divElem.insertAdjacentHTML('beforebegin',
-    '<h1>Cобытия input, copy, focus и blur.</h1>')
 divElem.insertAdjacentHTML('afterbegin',
+    '<h1>Cобытия input, copy, focus и blur.</h1>')
+divElem.insertAdjacentHTML('beforeend',
     `<p>Copy не дает явно скопировать текст, input показывает изменения немедленно, 
-focus и blur отвечают за фокусировку и расфокусировку.</p>`)
+focus и blur отвечают за фокусировку и расфокусировку.</p>
+<p>События blur и focus по умолчанию не всплывают. Но можно обойти через обработчик (focusin. focusout).</p>`)
 
 
 let inputElem = document.createElement('input');
@@ -19,7 +20,7 @@ inputElem.oninput = () => {
 }
 
 let spanElem = document.createElement('span')
-spanElem.style = 'padding-left:10px;font-size:22px;'
+spanElem.style = 'padding-left:20px;font-size:22px;'
 document.body.append(spanElem)
 
 // FOCUS, BLUR, COPY
@@ -34,3 +35,12 @@ inputElem.onblur = () => {
 document.body.oncopy = (e) => {
     e.preventDefault()
 }
+
+// всплытие
+inputElem.addEventListener('focusin', () => {
+    inputElem.classList.add('focused')
+})
+
+inputElem.addEventListener('focusout', () => {
+    inputElem.classList.remove('focused')
+})
--- input-focus-blur-copy/readme.md
+++ input-focus-blur-copy/readme.md
@@ -1,4 +1,2 @@
 This project contains an example of using INPUT, FOCUS, BLUR, COPY.  
-Press f12 to explore blur() and focus().
-
 Open main.js.
